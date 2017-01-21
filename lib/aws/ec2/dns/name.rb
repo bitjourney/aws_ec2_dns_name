b8beb367efa12e5eb89b2234e5771695f3b9ef14
Michikawa Masayoshi <michikawa.masayoshi@gmail.com>
2017-01-21 23:23:25 +0900

    Run `bundle gem aws-ec2-dns-name -t minitest`

---
+++ lib/aws/ec2/dns/name.rb
@@ -0,0 +1,11 @@
+require "aws/ec2/dns/name/version"
+
+module Aws
+  module Ec2
+    module Dns
+      module Name
+        # Your code goes here...
+      end
+    end
+  end
+end
---
+++ lib/aws/ec2/dns/name/version.rb
@@ -0,0 +1,9 @@
+module Aws
+  module Ec2
+    module Dns
+      module Name
+        VERSION = "0.1.0"
+      end
+    end
+  end
+end
